042b69e8b42dfc548ca666769229e51b120d9ecd
Khang <hellowdzyro@gmail.com>
2025-12-22 01:00:17 +0700

    update config 4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,24 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-// import * as Sentry from '@sentry/react-native';
+import * as Sentry from '@sentry/react-native';
 
-// Sentry.init({
-//   dsn: 'https://a128db18d525782ca69e9028b63dcc83@o4510502731579392.ingest.de.sentry.io/4510502733217872',
+Sentry.init({
+  dsn: 'https://a128db18d525782ca69e9028b63dcc83@o4510502731579392.ingest.de.sentry.io/4510502733217872',
 
-//   // Adds more context data to events (IP address, cookies, user, etc.)
-//   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-//   sendDefaultPii: true,
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
 
 
-//   // Configure Session Replay
-//   replaysSessionSampleRate: 0.1,
-//   replaysOnErrorSampleRate: 1,
-//   integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
 
-//   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-//   // spotlight: __DEV__,
-// });
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 
 export default function RootLayout() {
