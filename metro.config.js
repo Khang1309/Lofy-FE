2d513275be4efc558538c107eabd0252a54a7d99
Khang <hellowdzyro@gmail.com>
2025-12-22 15:49:02 +0700

    update metro config style/globalcss

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,5 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-config.watchFolders = [];
-config.server = {
-    enhanceMiddleware: (middleware) => middleware,
-};
-module.exports = withNativeWind(config, { input: './global.css' });
+
+module.exports = withNativeWind(config, { input: './styles/global.css' });
